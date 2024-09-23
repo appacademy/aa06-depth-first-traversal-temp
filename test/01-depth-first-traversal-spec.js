31fdfd1e3f449cf39dd3bfbe990e280410a465c5
daniel watson <ozzloy@each.do>
2024-09-22 19:29:34 -0700

    clarify spec wordings

--- test/01-depth-first-traversal-spec.js
+++ test/01-depth-first-traversal-spec.js
@@ -18,7 +18,7 @@ describe("printDepthFirst", function () {
     console.log = consoleLog;
   });
 
-  it("printDepthFirst(3) prints a DFS traversal from 3", function () {
+  it("printDepthFirst(3) prints a depth first traversal from 3", () => {
     printDepthFirst(3);
     expect(consoleLogArgs).to.be.deep.oneOf([
       [[3], [2], [1], [5], [4], [6]],
@@ -31,7 +31,7 @@ describe("printDepthFirst", function () {
     ]);
   });
 
-  it("printDepthFirst(6) prints a DFS traversal from 6", function () {
+  it("printDepthFirst(6) prints a depth first traversal from 6", () => {
     printDepthFirst(6);
     expect(consoleLogArgs).to.be.deep.oneOf([
       [[6], [4], [3], [2], [1], [5]],
@@ -42,7 +42,7 @@ describe("printDepthFirst", function () {
     ]);
   });
 
-  it("printDepthFirst(4) prints a DFS traversal from 4", function () {
+  it("printDepthFirst(4) prints a depth first traversal from 4", () => {
     printDepthFirst(4);
     expect(consoleLogArgs).to.be.deep.oneOf([
       [[4], [3], [2], [1], [5], [6]],
